f946f527291e1015ffd4f45eb04cb19d29faccfa
Sofia Rodrigues <sofia@algebraic.dev>
2023-07-23 20:58:17 -0300

    init: again x.x

--- crates/vulpi-syntax/src/lib.rs
+++
@@ -1,9 +0,0 @@
-#![allow(incomplete_features)]
-#![feature(specialization)]
-#![feature(associated_type_bounds)]
-
-pub mod r#abstract;
-pub mod concrete;
-pub mod elaborated;
-pub mod resolved;
-pub mod token;
