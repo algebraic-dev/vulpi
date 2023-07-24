6c30857df0636bc2876686f08adc044a39453ba2
Sofia Rodrigues <sofia@algebraic.dev>
2023-07-24 15:56:57 -0300

    feat: implement a single-shot interner for vulpi

--- crates/vulpi-intern/src/lib.rs
+++ crates/vulpi-intern/src/lib.rs
@@ -1,14 +1,43 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+//! The string interner of the compiler. It is used to store strings in a way that is more efficient
+//! than storing them directly. It is also used to make sure that strings are unique. This is
+//! important for the compiler because it allows us to compare strings by comparing their ids. None
+//! of the implementations implement the [Copy] trait because some of them are heavy to clone.
+
+#[cfg(feature = "single-shot")]
+pub mod no_rc;
+
+#[cfg(feature = "single-shot")]
+pub use no_rc::*;
+
+use std::marker::PhantomData;
+
+/// A interned symbol that contains a phantom data to make it unique.
+#[derive(Clone, PartialEq, Eq, Hash)]
+pub struct Interned<T>(Symbol, PhantomData<T>);
+
+impl<T> Interned<T> {
+    pub fn new(symbol: Symbol) -> Self {
+        Self(symbol, PhantomData)
+    }
+
+    pub fn get(&self) -> String {
+        self.0.get()
+    }
+
+    pub fn cast<U>(self) -> Interned<U> {
+        Interned::new(self.0)
+    }
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+pub trait Internable: Sized {
+    fn intern(&self) -> Interned<Self>;
+}
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+impl<T> std::fmt::Debug for Interned<T> {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        f.debug_tuple("Interned")
+            .field(&self.0)
+            .field(&self.1)
+            .finish()
     }
 }
---
+++ crates/vulpi-intern/src/no_rc.rs
@@ -0,0 +1,55 @@
+//! A simple string interner with no reference counting so it lives until the end of the program.
+
+use lazy_static::lazy_static;
+
+use std::collections::HashMap;
+use std::sync::atomic::{AtomicUsize, Ordering};
+use std::sync::RwLock;
+
+lazy_static! {
+    static ref INTERNER: Interner = Interner::default();
+}
+
+/// A symbol is a reference to a string inside the interner. It is used to compare strings by
+/// comparing their ids instead of comparing their content because it is more efficient (it makes
+/// the comparison an integer comparison instead of a string comparison).
+#[derive(Debug, Clone, PartialEq, Eq, PartialOrd, Ord, Hash)]
+pub struct Symbol(usize);
+
+impl Symbol {
+    pub fn intern(string: &str) -> Self {
+        INTERNER.intern(string)
+    }
+
+    pub fn get(&self) -> String {
+        INTERNER.get(self).unwrap()
+    }
+}
+
+#[derive(Default)]
+struct Interner {
+    id_to_string: RwLock<Vec<String>>,
+    string_to_id: RwLock<HashMap<String, Symbol>>,
+    counter: AtomicUsize,
+}
+
+impl Interner {
+    fn intern(&self, string: &str) -> Symbol {
+        if let Some(id) = self.string_to_id.read().unwrap().get(string) {
+            return id.clone();
+        }
+
+        let mut id_to_string = self.id_to_string.write().unwrap();
+        let mut string_to_id = self.string_to_id.write().unwrap();
+
+        let id = Symbol(self.counter.fetch_add(1, Ordering::SeqCst));
+        id_to_string.push(string.to_owned());
+        string_to_id.insert(string.to_owned(), id.clone());
+
+        id
+    }
+
+    fn get(&self, id: &Symbol) -> Option<String> {
+        self.id_to_string.read().unwrap().get(id.0).cloned()
+    }
+}
