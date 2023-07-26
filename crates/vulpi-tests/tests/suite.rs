c7467d6ef307ab1df3ee5b4876360022456aca63
Sofia Rodrigues <sofia@algebraic.dev>
2023-07-26 08:38:42 -0300

    test: fix some tests

--- crates/vulpi-tests/tests/suite.rs
+++ crates/vulpi-tests/tests/suite.rs
@@ -1,6 +1,42 @@
 #![feature(custom_test_frameworks)]
 #![test_runner(vulpi_tests::test_runner)]
 
+use vulpi_lexer::Lexer;
+use vulpi_parser::Parser;
+use vulpi_report::{
+    hash::HashReporter,
+    renderer::{classic::Classic, Reader, Renderer},
+    Report,
+};
+use vulpi_show::Show;
 use vulpi_tests::test;
+use vulpi_vfs::{real::RealFileSystem, FileSystem};
 
-test!("/suite/", |_file_name| { "Ok!".to_string() });
+test!("/suite", |file_name| {
+    let reporter = Report::new(HashReporter::new());
+    let cwd = std::env::current_dir().unwrap();
+
+    let mut vfs = RealFileSystem::new(cwd.clone());
+    let id = vfs.load(file_name).unwrap();
+    let source = vfs.read(id).unwrap();
+
+    let lexer = Lexer::new(source, id, reporter.clone());
+
+    let mut parser = Parser::new(lexer, id, reporter.clone());
+    let program = parser.program();
+
+    let report = reporter.all_diagnostics();
+
+    if !reporter.has_errors() {
+        program.show().to_string()
+    } else {
+        let mut writer = Reader::default();
+        let ctx = Classic::new(&vfs, cwd);
+
+        for diagnostic in report {
+            diagnostic.render(&ctx, &mut writer).unwrap();
+        }
+
+        writer.to_string()
+    }
+});
